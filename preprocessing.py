88b9926048783d3f72559e8dd9492340dbd794a7
bwitten99 <witten@stanford.edu>
2021-05-14 13:08:44 -0500

    preprocessing comments

--- preprocessing.py
+++ preprocessing.py
@@ -5,62 +5,46 @@ import os
 
 from tqdm import tqdm
 
-def process_data_all(folder_loc, folders, main_file):
+def process_data_all(folders, folder_main_loc="data/", main_file="2021_04"):
     """
-    Iteretes over all specified folders.
+    Iterates over all specified folders to process a dataframe for each group.
+    Data is then joined together and deduplicated.
+    params :
+            folders --> list of strings for each Kickstarter folder to process (currently labeled by date, i.e. "2021-04", etc.)
+            folder_main_loc --> string for the folder to store data files (default is data/)
+            main_file --> string name for our main folder (only necessary deduplicating data, e.g. we need a baseline)
     """
     data = []
-    for fold in folders:
-        folder_path = folder_loc + fold
-        files = sorted(os.listdir(folder_path))
-        df = process_data_folder(fold, folder_path, files, save=True)
-        print("Finished proccessing file {}".format(fold))
-    # remove duplicate rows
+    # double check that main_file has been included in list of folders to process
+    if main_file not in folders: folders.append(main_file)
+    for data_folder in folders:
+        folder_path = folder_main_loc + data_folder
+        data_folder_files = sorted(os.listdir(folder_path))
+        df = process_data_folder(folder_main_loc, data_folder, data_folder_files, save=True)
+        print("Finished proccessing file {}".format(data_folder))
     folders.remove(main_file)
-    final_df = unduplicate_data(folder_path, main_file, folders)
+    # remove duplicate projects
+    final_df = unduplicate_data(folder_main_loc, main_file, folders)
     return final_df
 
-def unduplicate_data(folder_path, main_file, addtl_files, features_dup=None, extension=".parquet.gzip"):
-    """
-    Removes duplicate projects from the list of dataframes.
-    Each dataframe does not include just the unique projects of that month-year combination, but rather all
-    projects the web robot finds when scraping Kickstarter. As a result, each dataframe consists of heavy
-    overlap. Ideally, we would remove these rows earlier in the pipeline, but for now this seems fine.
-    Currently, to drop duplicates we are selecting the following subset of features to identify :
-
-                    ["blurb", "name", "usd_goal", "launched_at", "deadline"]
-
-    types:
-        main_file --> string to path of main file (name, no extension necessary)
-        addtl_files --> list of string to path of addtl files to add (name, no extension necessary)
-        features_dup --> list of features to use to identify duplicate rows
-    """
-    # in order to remove duplicate rows, we need to start with a main file
-    main_data = pd.read_parquet(main_file + extension)
-    # initialize duplicate features
-    if features_dup is None: features_dup = ["blurb", "name", "usd_goal", "launched_at", "deadline"]
-    for new_file in addtl_files:
-        tmp = pd.read_parquet(new_file + extension)
-        # combine and drop duplicates
-        main_data = pd.concat((main_data, tmp)).drop_duplicates(subset=features_dup)
-        del tmp # not sure if this is fully necessary, but ease load
-        print("Adding file", new_file, "to yield new shape {}".format(main_data.shape))
-    main_data.to_parquet(folder_path + "/" +  "all_processed_df" + extension)
-    return main_data
-
-def process_data_folder(folder_name, file_loc, file_paths, save=False):
+def process_data_folder(folder_main_loc, folder_name, file_paths, save=True):
     """
     Iterates over all csvs in given folder.
+    params :
+            folder_main_loc --> string for the folder to store data files (e.g. "data/")
+            folder_name --> string name for the folder containing data we are operating on (e.g. "2021-04")
+            file_paths --> list of individual csv paths within folder name to process
+            save --> boolean indicating whether we save the file (default = True )
     """
     dfs = []
     for f in tqdm(file_paths):
         if ".csv" in f:
-            dfs.append( process_data_csv( file_loc + "/" +  f ) )
+            dfs.append( process_data_csv( folder_main_loc + "/" + folder_name + "/" + f ) )
         else:
             print("Error invalid file {}".format(f))
             print("Skipping to next.")
     fulldf = pd.concat(dfs)
-    if save: fulldf.to_parquet(file_loc + "/" + folder_name + ".parquet.gzip", compression="gzip")
+    if save: fulldf.to_parquet( folder_main_loc + "/" + folder_name + ".parquet.gzip", compression="gzip")
     return fulldf
 
 
@@ -101,9 +85,35 @@ def process_data_csv(file_path):
     ]
     return df[ df_cols_to_keep + cat_cols_to_keep + loc_cols_to_keep ]
 
+def unduplicate_data(folder_path, main_file, addtl_files, features_dup=None, extension=".parquet.gzip"):
+    """
+    Removes duplicate projects from the list of dataframes.
+    Each dataframe does not include just the unique projects of that month-year combination, but rather all
+    projects the web robot finds when scraping Kickstarter. As a result, each dataframe consists of heavy
+    overlap. Ideally, we would remove these rows earlier in the pipeline, but for now this seems fine.
+    Currently, to drop duplicates we are selecting the following subset of features to identify :
 
+                    ["blurb", "name", "usd_goal", "launched_at", "deadline"]
+
+    types:
+        main_file --> string to path of main file (name, no extension necessary)
+        addtl_files --> list of string to path of addtl files to add (name, no extension necessary)
+        features_dup --> list of features to use to identify duplicate rows
+    """
+    # in order to remove duplicate rows, we need to start with a main file
+    main_data = pd.read_parquet(main_file + extension)
+    # initialize duplicate features
+    if features_dup is None: features_dup = ["blurb", "name", "usd_goal", "launched_at", "deadline"]
+    for new_file in addtl_files:
+        tmp = pd.read_parquet(new_file + extension)
+        # combine and drop duplicates
+        main_data = pd.concat((main_data, tmp)).drop_duplicates(subset=features_dup)
+        del tmp # not sure if this is fully necessary, but ease load
+        print("Adding file", new_file, "to yield new shape {}".format(main_data.shape))
+    main_data.to_parquet(folder_path + "/" +  "all_processed_df" + extension)
+    return main_data
 
 
 if __name__ == '__main__':
     folders = ["2021_04", "2021_03", "2021_02", "2020_12", "2020_04"]
-    process_data_all("data/", folders, "2021_04")
+    process_data_all(folders, "data/",  "2021_04")
