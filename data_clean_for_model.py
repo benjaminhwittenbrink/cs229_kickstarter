18dade0f1841c31b8dee3beb7654ed25ebf38d91
bwitten99 <witten@stanford.edu>
2021-05-25 08:06:51 -0500

    add kfold mean encoding

--- data_clean_for_model.py
+++ data_clean_for_model.py
@@ -3,27 +3,29 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def data_clean_for_model(df, outcome, params):
+def data_clean_for_model(df, outcome, params, cv=True):
 
-    #X_train, X_test, y_train, y_test = train_test_split(dfs, dfs["outcome"], test_size=0.3, random_state=rseed)
-    # to avoid info leakage, we'll calculate the averages on a different df
-    X_train, X_lnom, X_test, y_train, y_lnom, y_test = split_data(df, outcome, params['lnom_frac'], params['test_frac'], params['rseed'])
-    # add outcome lnoms
-    X_train_lnom = add_lnoms(X_train, X_lnom, "outcome")
-    X_test_lnom = add_lnoms(X_test, X_lnom, "outcome")
-    # add usd_goal lnoms (let's ignore these for now --> don't help performance at all, if anything hurt it )
-    if params['lnom_usdgoal']:
-        X_train_lnom = add_lnoms(X_train_lnom, X_lnom, "usd_goal")
-        X_test_lnom = add_lnoms(X_test_lnom, X_lnom, "usd_goal")
+    if cv:
+        X_train, X_test, y_train, y_test = train_test_split(df, df[outcome], test_size=params['test_frac'], random_state=params['rseed'])
+        X_train_lnom, X_test_lnom = encode_categoricals(X_train, X_train, X_test, add_cv_lnoms, params)
+    else: 
+        X_train, X_lnom, X_test, y_train, y_lnom, y_test = split_data(df, outcome, params['lnom_frac'], params['test_frac'], params['rseed'])
+        X_train_lnom, X_test_lnom = encode_categoricals(X_train, X_lnom, X_test, add_lnoms, params)
+    
     # specify feature set here
     X_cols_all = specify_features(lnom_usdgoal=params['lnom_usdgoal'])
     # subset features on our train and test data
     X_train_lnom = X_train_lnom[X_cols_all]
     X_test_lnom = X_test_lnom[X_cols_all]
+    
     # make sure all values are floats, if na --> fill w/ 0.5 (i.e. we have no better guess)
     # there aren't many nas though, only really in cat_parent_id and loc_type
-    X_test_lnom = replace_nas(X_test_lnom, X_lnom, X_cols_all)
-    X_train_lnom = replace_nas(X_train_lnom, X_lnom, X_cols_all)
+    if cv: 
+        X_test_lnom = replace_nas(X_test_lnom, X_train, X_cols_all)
+        X_train_lnom = replace_nas(X_train_lnom, X_train, X_cols_all)
+    else:
+        X_test_lnom = replace_nas(X_test_lnom, X_lnom, X_cols_all)
+        X_train_lnom = replace_nas(X_train_lnom, X_lnom, X_cols_all)
     ## subtract actual usd_goal from category goal
     if params['lnom_usdgoal']:
         X_lnom_cols_usdgoal = X_train_lnom.columns[ X_train_lnom.columns.str.contains("usd_goal_mean") ]
@@ -34,7 +36,7 @@ def data_clean_for_model(df, outcome, params):
 
 def split_data(df, outcome, lnom_frac, test_frac, rseed):
 
-    X_train, X_lnom, y_train, y_lnom = train_test_split(df, df["outcome"], test_size=lnom_frac, random_state=rseed)
+    X_train, X_lnom, y_train, y_lnom = train_test_split(df, df[outcome], test_size=lnom_frac, random_state=rseed)
     X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=test_frac, random_state=rseed)
 
     return X_train, X_lnom, X_test, y_train, y_lnom, y_test
@@ -70,6 +72,16 @@ def replace_nas(df1, df2, df1_varlist, lnom_usdgoal=True):
     df1['blurb_len'] = df1['blurb_len'].fillna( df2['blurb_len'].mean() )
     return df1
 
+def encode_categoricals(X_train, X_lnom, X_test, add_lnoms_func, params):
+    # add outcome lnoms
+    X_train_lnom = add_lnoms_func(X_train, X_lnom, "outcome")
+    X_test_lnom = add_lnoms_func(X_test, X_lnom, "outcome")
+    # add usd_goal lnoms (let's ignore these for now --> don't help performance at all, if anything hurt it )
+    if params['lnom_usdgoal']:
+        X_train_lnom = add_lnoms_func(X_train_lnom, X_lnom, "usd_goal")
+        X_test_lnom = add_lnoms_func(X_test_lnom, X_lnom, "usd_goal")
+    return X_train_lnom, X_test_lnom
+
 def lnom(df, grp_col, target_col="outcome", min_n=1000):
     """
     Encode our categorical variables, by calculating the leave-nothing-out mean,
@@ -101,5 +113,40 @@ def add_lnoms(df1, df2, target_col="outcome"):
         lnom(df2, "loc_type", target_col), on="loc_type", how="left"
     ).merge(
         lnom(df2, "loc_id", target_col), on="loc_id", how="left"
+    )
+    return dfs_lnom
+
+def cv_lnom(df, grp_col, target_col="outcome", min_n=1000):
+    k = df['cv_group'].max()+1
+    out_col = grp_col + "_" + target_col + "_mean"
+    tmp_df = df.groupby([grp_col, "cv_group"])[target_col].agg(
+        [(out_col, "sum"), ("c", "count")]
+    ).reset_index()
+    tmp_df = pd.concat( (tmp_df.assign(cv_main = i) for i in range(k) ))
+    tmp_df = tmp_df.loc[ tmp_df['cv_main'] != tmp_df['cv_group'] ]
+    res = tmp_df.groupby([grp_col, "cv_main"]).agg('sum')
+    res[out_col] = res[out_col]/res['c']
+    res = res.loc[res['c']>min_n].drop(columns=['cv_group']).reset_index().rename(columns={'cv_main':'cv_group'})
+    return res[[grp_col, "cv_group", out_col]]
+
+def add_cv_lnoms(df1, df2, target_col="outcome"):
+    """
+    lnom = "Leave none out mean", i.e. calculate the average within a group. (as opposed to a loom -- "leave one out of mean")
+    Add lnoms for a bunch of categorical variables (currently hard-coded). 
+    Lnoms are calculated using df2 and then merged onto df1 (i.e. insuring indepdence, though don't think this all that important). 
+    Target column is specified with target_col. 
+    """
+    dfs_lnom = df1.merge(
+        cv_lnom(df2, "currency", target_col), on=["currency", "cv_group"], how="left"
+    ).merge(
+        cv_lnom(df2, "country", target_col), on=["country", "cv_group"], how="left"
+    ).merge(
+        cv_lnom(df2, "cat_id", target_col), on=["cat_id", "cv_group"], how="left"
+    ).merge(
+        cv_lnom(df2, "cat_parent_id", target_col), on=["cat_parent_id", "cv_group"], how="left"
+    ).merge(
+        cv_lnom(df2, "loc_type", target_col), on=["loc_type", "cv_group"], how="left"
+    ).merge(
+        cv_lnom(df2, "loc_id", target_col), on=["loc_id", "cv_group"], how="left"
     )
     return dfs_lnom
